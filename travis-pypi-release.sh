2d5d298af12a37cad4120c9356e8f033ab9e3aa7
Matthew Duck <matt@mattduck.com>
2020-07-19 10:22:57 +0100

    ci: reconfigure the deploy condition

--- travis-pypi-release.sh
+++ travis-pypi-release.sh
@@ -4,3 +4,4 @@ if [ "$(make assert_clean_git assert_new_pypi_version; echo $?)" == "0" ]; then
     make release
     git push --quiet https://mattduck:$GITHUB_TAG_TOKEN@github.com/mattduck/gh2md.git --tags >/dev/null 2>&1
 fi
+pip install gh2md --upgrade
