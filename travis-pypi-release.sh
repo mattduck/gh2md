f8fdebc3f22937cbc76195a6dc942f8f4737c00f
Matthew Duck <matt@mattduck.com>
2020-07-19 10:46:39 +0100

    ci: travis

--- travis-pypi-release.sh
+++ travis-pypi-release.sh
@@ -8,6 +8,7 @@ git config --global user.name "Travis CI"
 pip install -r dev-requirements.txt
 git status
 make assert_clean_git
+make assert_new_pypi_version || echo "pypi check failed, see logs above"
 if [ "$(make assert_new_pypi_version; echo $?)" == "0" ]; then
     make release
     git push --quiet https://mattduck:$GITHUB_TAG_TOKEN@github.com/mattduck/gh2md.git --tags >/dev/null 2>&1
